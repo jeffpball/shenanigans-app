28e50f47c696f6f9a00ea277f619e5e3d8154346
Loann Pham <loannpham87@gmail.com>
2020-01-02 23:44:45 -0800

    added onPress alerts and converted inline styling to stylesheet object

--- Shenanigans-Espresso-App/App.js
+++ Shenanigans-Espresso-App/App.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Shenanigans Espresso App</Text>
-      <Button title='Login' icon={{ name: 'code' }} />
-      <Button title='Signup' icon={{ name: 'code' }} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.header}>Shenanigans Espresso App</Text>
+				<View style={styles.buttons}>
+					<Button style={styles.buttons}
+						title='Login'
+						icon={{ name: 'code' }}
+						onPress={() => Alert.alert('Login button pressed')}
+					/>
+					<Button style={styles.buttons}
+						title='Signup'
+						icon={{ name: 'code' }}
+						onPress={() => Alert.alert('Signup button pressed')}
+					/>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	header: {
+    fontSize: 25,
+	},
+	buttons: {
+    margin: 20,
+	}
 });

340b0f8c7875150920775905bdfa2be0353dc2f3
jsevern94 <severnj@spu.edu>
2020-01-11 12:09:35 -0800

    debug for StyleSheet and createAppContainer'

--- Shenanigans-Espresso-App/navigation/InitialLoadingScreenNavigator.js
+++ Shenanigans-Espresso-App/navigation/InitialLoadingScreenNavigator.js
@@ -1,6 +1,7 @@
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
-import { createStackNavigator, createAppContainer } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import InitialLoadingScreen from '../screens/InitialLoadingScreen';
